f7ddce601f22aa2c93133d44129fb6c40afdde81
Nick Richard <nrich414@gmail.com>
2020-11-10 15:57:00 -0500

    Part 2 complete!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import PetDetailPage from "./PetDetailPage";
 
 function App() {
-    return <h1>Hello programmers!</h1>;
+	return <PetDetailPage></PetDetailPage>;
 }
 
 export default App;
---
+++ src/PetDetailPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Navigation from './Navigation.js'
+
+const PetDetailPage = () => <Navigation />
+
+
+export default PetDetailPage;
