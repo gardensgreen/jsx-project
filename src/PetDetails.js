f8596ea83bc8da1445400b322b9bb940d164233e
Daniel TIllero <danieltillero67@gmail.com>
2020-11-09 19:21:40 -0500

    finito

--- src/PetDetails.js
+++ src/PetDetails.js
@@ -1,5 +1,18 @@
 import React from "react";
 import PetDetailList from "./PetDetailList.js";
+import OwnersList from "./OwnersList";
 
-const PetDetails = (props) => <PetDetailList pet={props.pet} />;
+const PetDetails = (props) => (
+    <>
+        <PetDetailList pet={props.pet} />
+        <OwnersList owners={props.pet.Owners}></OwnersList>
+    </>
+);
+
+PetDetails.defaultProps = {
+    pet: {
+        PetType: {},
+    },
+};
+
 export default PetDetails;
